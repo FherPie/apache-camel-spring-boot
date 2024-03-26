6d7d8f83a3d0fa6133a7d3de5838e49870b633b2
FherPie <panaonchoatu@hotmail.com>
2024-03-26 10:01:26 -0500

    cambio

--- src/main/java/in/mpapp/springsecurityjwtdemo/constants/Constants.java
+++ src/main/java/in/mpapp/springsecurityjwtdemo/constants/Constants.java
@@ -2,5 +2,5 @@ package in.mpapp.springsecurityjwtdemo.constants;
 
 public class Constants {
     public static final String BEARER = "Bearer ";
-    
+    public static final String Route = "Bearer ";
 }
